de95ca9e5bd9d39bd3510a2c56480b8c2a6e0be8
Savitoj Singh <savitojs@gmail.com>
2025-07-11 23:21:24 -0400

    feat: minor improvements
    Signed-off-by: Savitoj Singh <savitojs@gmail.com>

---
+++ gtime/__main__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Entry point for running gtime as a module: python -m gtime
+"""
+
+from .cli import main
+
+if __name__ == "__main__":
+    main() 
